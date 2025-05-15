445afb8c9dfc81b67ea628a1172e06124dc2ca02
Lamora <itsmerick184@gmail.com>
2025-05-15 14:11:27 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+# Load .env
+load_dotenv()
+
+# Ambil variabel dari environment
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
