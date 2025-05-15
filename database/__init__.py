5918d0facc2d9873a1d550d713d68497b1afcadc
Lamora <itsmerick184@gmail.com>
2025-05-15 15:27:57 +0800

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,10 @@
-#dummies
+from .db import *
+
+class DB:
+    def __init__(self):
+        self.blacklist_users = blacklist_users
+        self.whitelist_users = whitelist_users
+        self.blacklist_words = blacklist_words
+        self.whitelist_words = whitelist_words
+
+db = DB()
