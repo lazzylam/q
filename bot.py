6d06da70dd1e073dc88f97096afae57c926d4526
Lamora <itsmerick184@gmail.com>
2025-05-15 16:02:14 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,6 @@ client = TelegramClient("lamora", API_ID, API_HASH)
 
 async def main():
     await client.start(bot_token=BOT_TOKEN)
-    me = await client.get_me()
-    logging.info(f"Bot @{me.username} berjalan.")
-    await client.run_until_disconnected()
-
-if __name__ == '__main__':
-    asyncio.run(main())
-
-async def main():
-    await client.start(bot_token=BOT_TOKEN)
     ankes.register(client)
     com.register(client)
     print("aktif!")
