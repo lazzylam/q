2967e43b8442023cb1b0070af887e476eae24613
Lamora <itsmerick184@gmail.com>
2025-05-15 16:05:26 +0800

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,8 @@
-from . import ankes
-from . import com
+from .ankes import register as register_ankes
+from .com import register as register_com
 
-__all__ = ["ankes", "com"]
+__all__ = ["register"]
+
+def register(client):
+    register_ankes(client)
+    register_com(client)
